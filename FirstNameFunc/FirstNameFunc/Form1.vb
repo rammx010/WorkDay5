7c5abb5e281597cd4670e496b3f2fc378a8e3043
idsc3101 <idsc3101@gmail.com>
2013-10-07 14:57:51 -0500

    questions 1 and 2

--- FirstNameFunc/FirstNameFunc/Form1.vb
+++ FirstNameFunc/FirstNameFunc/Form1.vb
@@ -3,13 +3,29 @@
 Public Class Form1
 
     Private Sub btnDetermine_Click(sender As System.Object, e As System.EventArgs) Handles btnDetermine.Click
+        ' not uncrashable: thinking that we have nicest possible user
         Dim name As String
+
         name = txtFullName.Text
+
+        'used for step through process
+        Dim result As String
+        result = FirstName(name)
         txtFirstName.Text = FirstName(name)
+
     End Sub
 
     Function FirstName(ByVal name As String) As String
-        Return "Not Implemented Yet"
+        'find the first  space
+        Dim firstspace As Integer
+        firstspace = name.IndexOf(" ")
+
+        'extract the first name
+        Dim Fname As String
+        Fname = name.Substring(0, firstspace)
+
+        Return Fname
+
     End Function
 
 End Class
